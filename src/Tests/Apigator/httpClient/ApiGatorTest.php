4b790a149223d4e676f7f28dfac0eac65c37cb21
Pascual <pascual.munoz@pccomponentes.com>
2018-04-03 21:28:55 +0200

    initial commits

---
+++ src/Tests/Apigator/httpClient/ApiGatorTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pascual
+ * Date: 01/04/2018
+ * Time: 16:38
+ */
+
+namespace Apigator\httpClient;
+
+
+class ApiGatorTest extends \PHPUnit_Framework_TestCase
+{
+
+}
